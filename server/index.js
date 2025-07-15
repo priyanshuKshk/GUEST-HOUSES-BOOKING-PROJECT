b398e39644e12b4c8098064a16a768000f61afd1
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-07-15 16:34:07 +0530

    fix(CORS): remove trailing slash from Vercel origin for correct CORS configuration

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://guest-houses-booking-project.vercel.app/",
+  "https://guest-houses-booking-project.vercel.app",
   "https://guest-houses-booking-project.onrender.com"
   
 ];
